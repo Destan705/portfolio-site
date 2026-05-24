a5546badf93e016d5822ec44f1bdee6dff73a932
Destan Cakar <destanckr@gmail.com>
2026-05-24 16:14:36 -0400

    updated style

--- project_1/src/pages/home/home.jsx
+++ project_1/src/pages/home/home.jsx
@@ -12,7 +12,10 @@ import analyticsSite from "./../../assets/image/analytics_site.png";
 
 function Home() {
   return (
-    <div style={{ backgroundColor: "PaleTurquoise" }}>
+    <div
+      className="bg-transparent m-2 p-2"
+      style={{ backgroundColor: "PaleTurquoise" }}
+    >
       <img
         src={portfolioImg}
         style={{ borderRadius: "50%", border: " 5px" }}
@@ -25,28 +28,34 @@ function Home() {
       <h2>Aspiring Software Engineer</h2>
       <div class="d-flex flex-column align-items-center justify-content-center">
         <div
-          className="w-50 mb-3"
-          style={{ backgroundColor: "SkyBlue", borderRadius: "5%" }}
+          className="w-50 mb-3 pb-1 mx-auto"
+          style={{ backgroundColor: "SkyBlue", borderRadius: "20px" }}
         >
           <h3>About Me</h3>
-          <ul>
-            <li>I like working with full stack web development.</li>
-            <li>I am interested in AI and it's role in the future.</li>
-            <li>My favorite language is Phyton.</li>
+          <ul className="mb-0">
+            <li style={{ marginBottom: "0" }}>
+              I enjoy building responsive and interactive web applications.
+            </li>
+            <li style={{ marginBottom: "0" }}>
+              I am passionate about AI and its impact on the general public.
+            </li>
+            <li style={{ marginBottom: "0" }}>
+              My favorite programming language to work with is Python.
+            </li>
           </ul>
         </div>
         <div
           className="w-50 mb-3"
-          style={{ backgroundColor: "PowderBlue", borderRadius: "5%" }}
+          style={{ backgroundColor: "PowderBlue", borderRadius: "20px" }}
         >
           <h3>My Experince:</h3>
-          <ul>
+          <ul className="mb-0">
             <li>
               I am currently taking SEG 3125 (Analysis and Design of User
               Interfaces) as part of my software engineering squence.
             </li>
             <li>
-              you can find out more about this course through this link:{" "}
+              You can find out more about this course through this link:{" "}
               <a
                 href="https://catalogue.uottawa.ca/en/courses/seg/"
                 target="_blank"
@@ -60,50 +69,60 @@ function Home() {
         </div>
       </div>
 
-      <div style={{ backgroundColor: "LightCyan", borderRadius: "5%" }}>
-        <Link to="/onhold">
-          <img
-            src={seriveSite}
-            class="bg-transparent m-1 p-1"
-            style={{ borderRadius: "10%" }}
-            width="250px"
-            height="250px"
-            alt="seriveSit"
-          />
-        </Link>
+      <div
+        style={{ backgroundColor: "LightCyan", borderRadius: "20px" }}
+        className="p-3"
+      >
+        <h3>Projects:</h3>
+        <div className="row justify-content-center">
+          <div className="col-6 col-md-3 d-flex flex-column align-items-center mb-3">
+            <Link to="/onhold">
+              <img
+                src={seriveSite}
+                className="img-fluid rounded"
+                style={{ width: "250px", height: "180px", objectFit: "cover" }}
+                alt="seriveSite"
+              />
+            </Link>
+            <p>A Service Site</p>
+          </div>
 
-        <Link to="/onhold">
-          <img
-            src={memoryGame}
-            class="bg-transparent m-1 p-1"
-            style={{ borderRadius: "10%" }}
-            width="250px"
-            height="250px"
-            alt="memoryGame"
-          />
-        </Link>
+          <div className="col-6 col-md-3 d-flex flex-column align-items-center mb-3">
+            <Link to="/onhold">
+              <img
+                src={memoryGame}
+                className="img-fluid rounded"
+                style={{ width: "250px", height: "180px", objectFit: "cover" }}
+                alt="memoryGame"
+              />
+            </Link>
+            <p>Memory Game</p>
+          </div>
 
-        <Link to="/onhold">
-          <img
-            src={ecommerceSite}
-            class="bg-transparent m-1 p-1"
-            style={{ borderRadius: "10%" }}
-            width="250px"
-            height="250px"
-            alt="ecommerceSite"
-          />
-        </Link>
+          <div className="col-6 col-md-3 d-flex flex-column align-items-center mb-3">
+            <Link to="/onhold">
+              <img
+                src={ecommerceSite}
+                className="img-fluid rounded"
+                style={{ width: "250px", height: "180px", objectFit: "cover" }}
+                alt="ecommerceSite"
+              />
+            </Link>
+            <p>An E-commerce Site</p>
+          </div>
 
-        <Link to="/onhold">
-          <img
-            src={analyticsSite}
-            class="bg-transparent m-1 p-1"
-            style={{ borderRadius: "10%" }}
-            width="250px"
-            height="250px"
-            alt="analyticsSite"
-          />
-        </Link>
+          <div className="col-6 col-md-3 d-flex flex-column align-items-center mb-3">
+            <Link to="/onhold">
+              <img
+                src={analyticsSite}
+                className="img-fluid rounded"
+                style={{ width: "250px", height: "180px", objectFit: "cover" }}
+                alt="analyticsSite"
+              />
+            </Link>
+            <p>An Analytics Site</p>
+          </div>
+        </div>
       </div>
     </div>
   );
